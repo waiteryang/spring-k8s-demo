e1cf2c037ad3c1039cfbe036f16eede643dcdd44
yangli <1454825165@qq.com>
2019-09-10 20:43:00 +0800

    LRU实现,利用linkedhashmap

--- src/main/java/com/designpattern/demo/lru/LRULinkedMap.java
+++ src/main/java/com/designpattern/demo/lru/LRULinkedMap.java
@@ -16,3 +16,15 @@ public class LRULinkedMap<K,V> extends LinkedHashMap<K,V> {
         }
     };
 }
+
+//    //LinkedHashMap的一个构造函数，当参数accessOrder为true时，即会按照访问顺序排序，最近访问的放在最前，最早访问的放在后面
+//    public LinkedHashMap(int initialCapacity, float loadFactor, boolean accessOrder) {
+//        super(initialCapacity, loadFactor);
+//        this.accessOrder = accessOrder;
+//    }
+//
+//    //LinkedHashMap自带的判断是否删除最老的元素方法，默认返回false，即不删除老数据
+////我们要做的就是重写这个方法，当满足一定条件时删除老数据
+//    protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+//        return false;
+//    }
