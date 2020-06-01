4b9db96a54ff5e7f24105aa1fb6c040465c15831
yangli <1454825165@qq.com>
2020-06-01 17:23:52 +0800

    feat:新增单例模式

---
+++ design_pattern/src/main/java/com/yangli/design_pattern/singleton/HungrySingleton.java
@@ -0,0 +1,31 @@
+package com.yangli.design_pattern.singleton;
+
+
+/**
+ * 单例-饿汉模式
+ * 一单加载就创建实例
+ * @author yangli
+ */
+public class HungrySingleton {
+
+
+    /**
+     * 创建私有静态实例，意味着这个类第一次使用的时候就会进行创建
+     */
+    private static final HungrySingleton instance = new HungrySingleton();
+
+
+    /**
+     * 构造函数
+     */
+    public HungrySingleton(){
+
+    }
+
+    /**
+     * 获取单例对象
+     */
+    public static HungrySingleton getInstance(){
+        return instance;
+    }
+}
--- design_pattern/src/main/java/com/yangli/design_pattern/singleton/LazySingleton.java
+++ design_pattern/src/main/java/com/yangli/design_pattern/singleton/LazySingleton.java
@@ -1,4 +1,54 @@
 package com.yangli.design_pattern.singleton;
 
+/**
+ * 懒加载-单例模式
+ * 当调用时才创建实例
+ * @author yangli
+ */
 public class LazySingleton {
+
+
+    /**
+     * 保证instance 在所有线程中都同步
+     * 和饿汉模式相比，这边不需要先实例化出来，注意这里的volatile，它是必须的
+     */
+    private static volatile LazySingleton instance = null;
+
+    /**
+     * 避免外部被实例化
+     */
+    public LazySingleton(){
+
+    }
+
+    /**
+     * 减小锁的代码块
+     */
+    public static LazySingleton getInstance1(){
+
+        if (instance == null){
+            //加锁
+            synchronized (LazySingleton.class){
+                //这次判断也是必须的，不然会有并发问题
+                if (instance == null) {
+                    instance = new LazySingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+
+    /**
+     * 将整个函数加锁
+     */
+    public static synchronized  LazySingleton getInstance(){
+
+        if (instance == null){
+            return new LazySingleton();
+        }
+        return instance;
+    }
+
+
 }
