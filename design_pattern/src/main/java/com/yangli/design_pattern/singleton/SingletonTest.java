289fb532b4c0912642cb2834bf2e9b2944b464e5
yangli <1454825165@qq.com>
2020-06-11 21:30:18 +0800

    feat:单例模式增加多线程测试

--- design_pattern/src/main/java/com/yangli/design_pattern/singleton/SingletonTest.java
+++ design_pattern/src/main/java/com/yangli/design_pattern/singleton/SingletonTest.java
@@ -2,13 +2,14 @@ package com.yangli.design_pattern.singleton;
 
 /**
  * 单例测试类
- * @author yangli
  *
+ * @author yangli
+ * <p>
  * 单例的特定
  * 1.单例只有一个实例对象
  * 2.该单例对象必须由该类创建
  * 3.单例对外提供一个单例的全局访问点
- *
+ * <p>
  * 应用场景
  * 1.在应用场景中，某类只要求生成一个对象的时候，如一个班中的班长，每个人的身份证号等
  * 2.当对象需要被共享的场合，由于单例模式只允许创建一个对象，共享该对象可以节省内存，并加快对象访问速度。如web中的配置对象，数据库的连接池等。
@@ -20,10 +21,15 @@ public class SingletonTest {
     /**
      *
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
 
+        for (int i = 0; i < 10000L; i++) {
+
+            new Thread(()->{
+                System.out.println(LazySingleton.getInstance().hashCode());
+            }).start();
+        }
 
     }
 
