d3e42ece07d2e0e9fd27345bc7d4c121a00e03e4
yangli <1454825165@qq.com>
2020-06-21 14:59:52 +0800

    feat:增加相关注释

--- design_pattern/src/main/java/com/yangli/design_pattern/proxy/dynamicproxy/JDKTest.java
+++ design_pattern/src/main/java/com/yangli/design_pattern/proxy/dynamicproxy/JDKTest.java
@@ -2,7 +2,13 @@ package com.yangli.design_pattern.proxy.dynamicproxy;
 
 import com.yangli.design_pattern.proxy.ISubject;
 import com.yangli.design_pattern.proxy.TargetSubject;
-
+/**
+ * JDK代理
+ * JDK的动态代理有一个限制，就是使用动态代理对象必须实现一个或者多个接口，如果想代理没有实现接口的类，可以使用cglib实现
+ * 静态代理在编译时就实现，编译完成后代理类是一个实际的class文件
+ *
+ * 优点:可以在不修改目标对象的前提下扩展目标对象的功能
+ */
 public class JDKTest {
 
 
