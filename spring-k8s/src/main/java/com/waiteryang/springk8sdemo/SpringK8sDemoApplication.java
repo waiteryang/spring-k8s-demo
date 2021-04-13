9243ddeed4b4544ceb2937854eedf4ed9252e5ac
yangli <1454825165@qq.com>
2021-04-13 17:37:02 +0800

    feat:添加测试接口

---
+++ spring-k8s/src/main/java/com/waiteryang/springk8sdemo/SpringK8sDemoApplication.java
@@ -0,0 +1,13 @@
+package com.waiteryang.springk8sdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringK8sDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringK8sDemoApplication.class, args);
+    }
+
+}
