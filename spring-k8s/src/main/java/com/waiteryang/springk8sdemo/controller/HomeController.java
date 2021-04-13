098e749dfac3a9e3f1c9e90afc0baf083d0f7490
yangli <1454825165@qq.com>
2021-04-13 17:30:11 +0800

    feat:添加测试接口

--- spring-k8s/src/main/java/com/waiteryang/springk8sdemo/controller/HomeController.java
+++ spring-k8s/src/main/java/com/waiteryang/springk8sdemo/controller/HomeController.java
@@ -1,9 +1,18 @@
 package com.waiteryang.springk8sdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author yangli
  */
-@RequestMapping(value = "/v1/notify")
+@RequestMapping(value = "/v1/")
 @RestController
 public class HomeController {
+
+    @GetMapping(value = "/test")
+    public String wxPayNotify() {
+        return "我是测试呀";
+    }
 }
